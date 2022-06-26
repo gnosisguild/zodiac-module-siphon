f36f9e6a4248118b4c493ee738dd6499766e4b42
Auryn Macmillan <hi@auryn.xyz>
2022-06-26 21:00:24 +1000

    add README and CI

--- .solcover.js
+++ .solcover.js
@@ -1,18 +1,6 @@
 module.exports = {
   skipFiles: [
-    "test/*",
-    "test/TestAvatar.sol",
-    "test/TestToken.sol",
-    "test/TestContract.sol",
-    "test/TestPluckParam.sol",
-    "test/TestFactory.sol",
-    "test/MultiSend.sol",
-    "test/maker/DssProxy.sol",
-    "test/maker/DssProxyActions.sol",
-    "test/maker/Join.sol",
-    "test/maker/mockCDPManager.sol",
-    "test/maker/mockSpot.sol",
-    "test/maker/mockVat.sol",
+    "test/"
   ],
   mocha: {
     grep: "@skip-on-coverage", // Find everything with this tag
