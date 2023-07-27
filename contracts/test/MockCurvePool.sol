50439112af47bef479beaf995648b268d2893870
cristovaoth <cristovao.honorato@gmail.com>
2023-07-27 16:42:12 +0100

    ConvexCompoundAdapter price calculations tests

--- contracts/test/MockCurvePool.sol
+++ contracts/test/MockCurvePool.sol
@@ -3,4 +3,27 @@ interface CurvePool {
         uint256[2] memory amounts,
         bool deposit
     ) external view returns (uint256);
+
+    function balances(int128 i) external view returns (uint256);
+
+    function exchange(int128 i, int128 j, uint256 dx, uint256 minDy) external;
+
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 minDy
+    ) external;
+
+    function get_dx_underlying(
+        int128 i,
+        int128 j,
+        uint256 dy
+    ) external view returns (uint256);
+
+    function get_dy_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx
+    ) external view returns (uint256);
 }
