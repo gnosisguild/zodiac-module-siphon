91905ef8c0e1b4d267c3f8bb5ded7fb822f4b0f1
cristovaoth <cristovao.honorato@gmail.com>
2023-07-29 13:56:12 +0100

    Adding placeholders for MakerDaiVault tests

---
+++ test/dp/maker/MakerDaiVault.spec.ts
@@ -0,0 +1,38 @@
+import { BigNumber } from "ethers";
+import hre from "hardhat";
+import { fork, forkReset } from "../../setup";
+
+describe("MakerDaiVaultAdapter", async () => {
+  before(async () => {
+    await fork(17741542);
+  });
+
+  after(async () => {
+    await forkReset();
+  });
+
+  async function setup() {
+    const [signer] = await hre.ethers.getSigners();
+    // const safe = await highjack(GNO_SAFE, signer.address);
+
+    const Adapter = await hre.ethers.getContractFactory("MakerDaiVaultAdapter");
+
+    const adapter = await Adapter.deploy(
+      "0xD758500ddEc05172aaA035911387C8E0e789CF6a",
+      "0x0DA0C3e52C977Ed3cBc641fF02DD271c3ED55aFe",
+      BigNumber.from("5861789575712043409055978289"),
+      BigNumber.from("5382188610426512584678670975"),
+      27353
+    );
+
+    return { adapter };
+  }
+
+  it("does not need rebalancing when above ratioTrigger");
+
+  it("gets payment instructions to bring ratio back to ratioTarget");
+
+  it("depositing more than requested brings ratio above ratioTarget");
+
+  it("depositing less than requested brings ratio bellow ratioTarget");
+});
---
+++ test/setup.ts
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+import hre from "hardhat";
+
+export async function fork(blockNumber: number): Promise<void> {
+  // Load environment variables.
+  dotenv.config();
+  const { ALCHEMY_KEY } = process.env;
+  // fork mainnet
+  await hre.network.provider.request({
+    method: "hardhat_reset",
+    params: [
+      {
+        forking: {
+          jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+          blockNumber,
+        },
+      },
+    ],
+  });
+}
+
+export async function forkReset(): Promise<void> {
+  await hre.network.provider.request({
+    method: "hardhat_reset",
+    params: [],
+  });
+}
